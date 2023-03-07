2a0d36a4d465184d606152b7d5d7d2ba97a4406e
crushedace <kingcobra.kc22@gmail.com>
2023-03-07 11:52:21 +1030

    test docyment 1

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
+import javax.lang.model.element.Name;
 import java.util.Scanner;
 
 public class Main{
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("this is a test file ");
+        System.out.println("what is your name ?");
+        String name =scanner.nextLine();
+        System.out.println("hello "+name+" im testbot ");
+
+
     }
 }
 
