12318cee84511c81ef0538e5d144331b38f12b89
Cameron Swiggs <swiggy_13@hotmail.com>
2023-03-07 12:05:44 +1030

    Changed System.out.println and added "or is it"

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("this is a test file ");
+        System.out.println("this is a test file or is it ");
         System.out.println("what is your name ?");
         String name =scanner.nextLine();
         System.out.println("hello "+name+" im testbot ");
